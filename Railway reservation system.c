be06094ba7063991aa4d1dfef5ff40d74168103a
omprakashbabu <116138027+omprakashbabu@users.noreply.github.com>
2024-06-05 08:53:31 +0530

    Update Railway reservation system.c

--- Railway reservation system.c
+++ Railway reservation system.c
@@ -6,7 +6,7 @@ int trainNumber;
 string trainName; 
 string source; 
 string destination;  
-int availableSeats[3]; 
+int availableSeats[3];  
 public: 
 void setTrainDetails(int tNo, string tName, string src, string dest, int seats[3]) {
 trainNumber = tNo; 
